ded087ddd593473c6da698faf2cf0905899e836d
sneha <goyalsneha995@gmail.com>
2024-08-27 23:25:10 +0530

    code

--- CPP codes/array.cpp
+++ CPP codes/array.cpp
@@ -1,14 +1,2 @@
 // Write C++ program to find the largest element of a given array of integers.
 
-#include<iostream>
-using namespace std;
-
-int main(){
-    int nums[]={5,78,23,45,90};
-    int n = sizeof(nums) / sizeof(nums[0]); // Determining the number of elements in the array
-cout << "Original array:"; // Output message indicating the original array is being displayed
-  for (int i=0; i < n; i++) // Loop to display each element of the array
-    cout << nums[i] <<" "; 
-   
-    return 0;
-}
