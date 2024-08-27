a29d344443087f612646c0ae88c0ed783c9f8663
sneha <goyalsneha995@gmail.com>
2024-08-27 23:21:20 +0530

    code

--- CPP codes/array.cpp
+++ CPP codes/array.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int amin(){
     int nums[]={5,78,23,45,90};
     int n = sizeof(nums) / sizeof(nums[0]); // Determining the number of elements in the array
-
+cout << "Original array:"; // Output message indicating the original array is being displayed
+  for (int i=0; i < n; i++) // Loop to display each element of the array
+    cout << nums[i] <<" "; 
     return 0;
 }
