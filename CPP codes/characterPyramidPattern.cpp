229507488faf79b00589a140303d9f6d71ef8b64
sneha <goyalsneha995@gmail.com>
2024-08-29 21:59:58 +0530

    code

--- CPP codes/characterPyramidPattern.cpp
+++ CPP codes/characterPyramidPattern.cpp
@@ -6,6 +6,12 @@ GHIJ*/
 #include<iostream>
 using namespace std;
 int main(){
-    
+    char ch = 'A';
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+cout<<ch++;
+        }
+        cout<<endl;
+    }
     return 0;
 }
